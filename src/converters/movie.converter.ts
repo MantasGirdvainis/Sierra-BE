cb904e6d42b07f65b31b996d5bf7fc5accd76eb1
Mantas Girdvainis <mantas.girdvainis@outlook.com>
2022-10-28 11:19:32 +0300

    updated

--- src/converters/movie.converter.ts
+++ src/converters/movie.converter.ts
@@ -1,12 +1,14 @@
-const converterToMovie = (tmdbMovie: TmdbMovie): Movie => {
+const urlAndSize = 'https://image.tmdb.org/t/p/w500';
+
+const convertToMovie = (tmdbMovie: TmdbMovie): Movie => {
   return {
     movieId: tmdbMovie.id,
     title: tmdbMovie.title,
     releaseDate: tmdbMovie.release_date,
-    backdropPath: 'https://image.tmdb.org/t/p/w500' + tmdbMovie.backdrop_path,
-    posterPath: 'https://image.tmdb.org/t/p/w500' + tmdbMovie.poster_path,
+    backdropPath: urlAndSize + tmdbMovie.backdrop_path,
+    posterPath: urlAndSize + tmdbMovie.poster_path,
     voteAverage: tmdbMovie.vote_average,
   };
 };
 
-export { converterToMovie };
+export { convertToMovie };
