fecff324e2bcca8e2b84be180e10a01a742b71ae
Mantas Girdvainis <mantas.girdvainis@outlook.com>
2022-11-09 18:38:44 +0200

    integrated with TMDB

--- src/controllers/movies.controller.ts
+++ src/controllers/movies.controller.ts
@@ -1,44 +1,13 @@
 import express from 'express';
 
-const getMovies = (_req: express.Request, res: express.Response): void => {
-  res.json({
-    page: 1,
-    totalPages: 1,
-    movies: [
-      {
-        movieId: 550988,
-        title: 'Free Guy',
-        releaseDate: '2021-08-11',
-        backdropPath: 'https://image.tmdb.org/t/p/w500/8Y43POKjjKDGI9MH89NW0NAzzp8.jpg',
-        posterPath: 'https://image.tmdb.org/t/p/w500/xmbU4JTUm8rsdtn7Y3Fcm30GpeT.jpg',
-        voteAverage: 7.8,
-      },
-      {
-        movieId: 335983,
-        title: 'Venom',
-        releaseDate: '2018-09-28',
-        backdropPath: 'https://image.tmdb.org/t/p/w500/VuukZLgaCrho2Ar8Scl9HtV3yD.jpg',
-        posterPath: 'https://image.tmdb.org/t/p/w500/2uNW4WbgBXL25BAbXGLnLqX71Sw.jpg',
-        voteAverage: 6.8,
-      },
-      {
-        movieId: 566525,
-        title: 'Shang-Chi and the Legend of the Ten Rings',
-        releaseDate: '2021-09-01',
-        backdropPath: 'https://image.tmdb.org/t/p/w500/dNQTwFmEj1b5SktT8oWGH5Cunlx.jpg',
-        posterPath: 'https://image.tmdb.org/t/p/w500/1BIoJGKbXjdFDAqUEiA2VHqkK1Z.jpg',
-        voteAverage: 7.8,
-      },
-      {
-        movieId: 436969,
-        title: 'The Suicide Squad',
-        releaseDate: '2021-07-28',
-        backdropPath: 'https://image.tmdb.org/t/p/w500/jlGmlFOcfo8n5tURmhC7YVd4Iyy.jpg',
-        posterPath: 'https://image.tmdb.org/t/p/w500/kb4s0ML0iVZlG6wAKbbs9NAm6X.jpg',
-        voteAverage: 7.8,
-      },
-    ],
-  });
+import * as movieService from '../services/movie.service';
+
+const getMovies = async (_req: express.Request, res: express.Response, next: express.NextFunction): Promise<void> => {
+  try {
+    res.json(await movieService.getMovies());
+  } catch (err) {
+    next(err);
+  }
 };
 
 const getMovie = (_req: express.Request, res: express.Response): void => {
