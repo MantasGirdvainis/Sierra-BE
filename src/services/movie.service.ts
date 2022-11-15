5c0e92c352af7f29e1d0dffbfd09b1d9c75511c3
Mantas Girdvainis <mantas.girdvainis@outlook.com>
2022-11-15 19:52:07 +0200

    cache created

--- src/services/movie.service.ts
+++ src/services/movie.service.ts
@@ -1,17 +1,26 @@
 import axios from "axios";
 import { convertToMovie } from "../converters/movie.converter";
 
-const getMovies = async (): Promise<Movies> => {
-    const { data } = await axios.get<TmdbMovies>(
-        `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&page=1&vote_count.gte=1000&api_key=${process.env.API_KEY}`,
-    );
+let moviesCahce: Movie[] | undefined;
+let totalPagesCache: number | undefined;
 
-    const movies: Movie[] = data.results.map(convertToMovie)
+const getMovies = async (): Promise<Movies> => {
+
+    if (!moviesCahce) {
+
+        const { data } = await axios.get<TmdbMovies>(
+            `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&page=1&vote_count.gte=1000&api_key=${process.env.API_KEY}`,
+        );
+        
+        moviesCahce = data.results.map(convertToMovie);
+        totalPagesCache = data.total_pages;
+    }
+    
 
     return {
         page: 1,
-        movies,
-        totalPages: data.total_pages,
+        movies: moviesCahce || [],
+        totalPages: totalPagesCache || 1
       };
 };
 
